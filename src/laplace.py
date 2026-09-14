f4277484253e7e936fe8faea6d4277d2ac71855c
NepSauce <zawadatif.builds@gmail.com>
2026-09-14 12:47:03 -0300

    Feat: Add block for generating plots

--- src/laplace.py
+++ src/laplace.py
@@ -10,7 +10,7 @@ t_max = 1.0 # Total sim time
 
 # Time array and initialize states
 
-time = np.arrange(0, t_max, dt)
+time = np.arange(0, t_max, dt)
 y = np.zeros_like(time)
 u = np.ones_like(time) # step input, turns on at t=0
 
@@ -21,3 +21,12 @@ for k in range(1, len(time)):
     dy = (1.0 / tau_time_constant) * ((k_gain * u[k-1]) - y[k-1])
     y[k] = y[k-1] + (dy * dt)
 
+# Plotting block
+
+plt.plot(time, y, label="System Output $y(t)$")
+plt.plot(time, u, "--", label="Step Input $u(t)$")
+plt.xlabel("Time (s)")
+plt.ylabel("Response")
+plt.legend()
+plt.grid(True)
+plt.show()
