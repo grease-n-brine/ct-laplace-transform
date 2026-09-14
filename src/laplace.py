73c5adaf9d19738507401c096405c1639eb3864d
NepSauce <zawadatif.builds@gmail.com>
2026-09-14 12:47:29 -0300

    Chore: Increase simulation time to 10 seconds

--- src/laplace.py
+++ src/laplace.py
@@ -6,7 +6,7 @@ import numpy as np
 k_gain = 1.0
 tau_time_constant = 2.0 # How sluggish the system is
 dt = 0.1 # Size of time step
-t_max = 1.0 # Total sim time
+t_max = 10.0 # Total sim time
 
 # Time array and initialize states
 
