e5f81b5d8d171d254ae3de87622688635eca699d
NepSauce <zawadatif.builds@gmail.com>
2026-09-14 12:43:40 -0300

    Fix: Add Euler's method in simulation loop

--- src/laplace.py
+++ src/laplace.py
@@ -3,7 +3,7 @@ import numpy as np
 
 # System parameters
 
-K_gain = 1.0
+k_gain = 1.0
 tau_time_constant = 2.0 # How sluggish the system is
 dt = 0.1 # Size of time step
 t_max = 1.0 # Total sim time
@@ -13,3 +13,11 @@ t_max = 1.0 # Total sim time
 time = np.arrange(0, t_max, dt)
 y = np.zeros_like(time)
 u = np.ones_like(time) # step input, turns on at t=0
+
+# Sim loop
+
+for k in range(1, len(time)):
+    # Euler's method derived from transfer function
+    dy = (1.0 / tau_time_constant) * ((k_gain * u[k-1]) - y[k-1])
+    y[k] = y[k-1] + (dy * dt)
+
