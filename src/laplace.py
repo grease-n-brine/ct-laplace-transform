305185894b1c88977ae7704be40d840b99767cba
NepSauce <zawadatif.builds@gmail.com>
2026-09-14 12:38:37 -0300

    Chore: Initialize parameters and imports

---
+++ src/laplace.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# System parameters
+
+K_gain = 1.0
+tau_time_constant = 2.0 # How sluggish the system is
+dt = 0.1 # Size of time step
+t_max = 1.0 # Total sim time
+
+
