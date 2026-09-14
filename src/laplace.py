d9e79f4a2c0adc07731d08d04b3a5100bc9648fb
NepSauce <zawadatif.builds@gmail.com>
2026-09-14 12:40:27 -0300

    Feat: Add time array and initial state initialization

--- src/laplace.py
+++ src/laplace.py
@@ -8,4 +8,8 @@ tau_time_constant = 2.0 # How sluggish the system is
 dt = 0.1 # Size of time step
 t_max = 1.0 # Total sim time
 
+# Time array and initialize states
 
+time = np.arrange(0, t_max, dt)
+y = np.zeros_like(time)
+u = np.ones_like(time) # step input, turns on at t=0
